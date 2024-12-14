9a89b629423d39c9cc8aa30be4581bddf6c54144
KamilR <runner27la@gmail.com>
2024-12-14 17:18:04 +0300

    Learning arrays, varargs, loops for, foreach

---
+++ src/com/kamilrr/java/git/ArraysExample.java
@@ -0,0 +1,46 @@
+package com.kamilrr.java.git;
+
+import java.util.Arrays;
+
+class StartArray{
+    public static void main(String[] args) {
+        ArraysExample.showArray();
+        VariableArgumentsExample.demonstrateVarargs(5,6,9,7,648,684,9);
+        ForEachLoopExample.demonstrateForEachLoop("Hello! ","Java ","World");
+    }
+}
+public class ArraysExample {
+    static void showArray(){
+        int[] array1 = new int[5];
+        array1[0] = 24;
+        array1[1] = 6486;
+        array1[2] = 96984;
+        array1[3] = 66;
+        System.out.println(Arrays.toString(array1));
+
+        String[] array2 = {"Alex","Malex","Lora","Milora"};
+        System.out.println(Arrays.toString(array2));
+
+        double[] array3 = new double[]{15.2,454.54,456.4};
+        System.out.println(Arrays.toString(array3));
+    }
+}
+class VariableArgumentsExample{
+    static void demonstrateVarargs(int... values){
+        int sum = 0;
+        for(int i = 0; i < values.length; i++){
+            sum += values[i];
+        }
+        double avgArrayValues = (double)sum / values.length;
+        System.out.println(avgArrayValues);
+    }
+}
+class ForEachLoopExample{
+    static void demonstrateForEachLoop(String... args){
+        String statement = "";
+        for(String element : args){
+            statement += element;
+        }
+        System.out.print(statement);
+    }
+}
---
+++ src/com/kamilrr/java/git/StringTemplatesExample.java
@@ -0,0 +1,26 @@
+package com.kamilrr.java.git;
+
+import static java.lang.StringTemplate.STR;
+
+class RunClass {
+    public static void main(String[] args) {
+        StringTemplatesExample user1 = new StringTemplatesExample();
+        user1.firstName = "Leonid";
+        user1.lastName = "Popov";
+        user1.age = 40;
+        String user1Info = user1.returnUserInfo(user1.firstName, user1.lastName, user1.age);
+        System.out.println(user1Info);
+    }
+}
+public class StringTemplatesExample {
+    String firstName, lastName;
+    short age;
+    String returnUserInfo(String firstName, String lastName, short age){
+        return STR."""
+                User information:
+                    First name: \{firstName}
+                    Last name: \{lastName}
+                    Age: \{age}
+                """;
+    }
+}
